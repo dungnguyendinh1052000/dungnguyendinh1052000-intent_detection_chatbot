2b2b2f24bfb96b5a60765bf2f2fa74e0fab28f04
Dung Nguyen <46893761+dungnguyendinh1052000@users.noreply.github.com>
2019-07-18 23:51:48 +0700

    Update

--- get_time.py
+++ get_time.py
@@ -7,6 +7,6 @@ def get_time():
     client = ntplib.NTPClient()
     response = client.request('pool.ntp.org')
 
-    time_=str(time.localtime(response.tx_time)[3])+":"+str(time.localtime(response.tx_time)[4])+" "+str(time.localtime(response.tx_time)[2])+'/'+str(time.localtime(response.tx_time)[1])+'/'+str(time.localtime(response.tx_time)[0])
+    time_=str(time.localtime(response.tx_time)[3])+":"+str(time.localtime(response.tx_time)[4])+" Ngày "+str(time.localtime(response.tx_time)[2])+' Tháng '+str(time.localtime(response.tx_time)[1])+' Năm '+str(time.localtime(response.tx_time)[0])
     
     return time_
